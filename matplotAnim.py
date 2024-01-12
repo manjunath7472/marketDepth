48c0735f0a7700a5b60f7e0975a1a074f99bc5a0
manjunath7472 <31816736+manjunath7472@users.noreply.github.com>
2024-01-12 09:58:04 +0530

    Add files via upload

--- matplotAnim.py
+++ matplotAnim.py
@@ -10,7 +10,7 @@ with open('enctoken.txt', 'r') as wr:
 
 kite = KiteApp(enctoken=token)
 
-scriptName = "NFO:BANKNIFTY2411047600CE"
+scriptName = "NFO:BANKNIFTY24JANFUT"
 r = 25
 v1 = deque([0] * r, maxlen=r)
 v2 = deque([0] * r, maxlen=r)
@@ -18,11 +18,9 @@ v3 = deque([0] * r, maxlen=r)
 x = [i for i in range(r)]
 
     
-def giveyrange(ary):
-   
-    # Calculating 10% minimum and 10% maximum
-    min_value = min(ary) * 0.9 # 10% less than the value
-    max_value = max(ary) * 1.1 # 10% more than the value
+def giveyrange(ary,pmin,pmax):
+    min_value = ary[-1]*pmin
+    max_value = ary[-1]*pmax
 
     return min_value, max_value
 
@@ -34,9 +32,15 @@ def calculate_percentage_change(current, previous):
     pc = round(pc, 3)
     return pc
 
+global rangei,v1min,v1max,v2min,v2max,v3min,v3max
+rangei=0
+
+
 # Combined update function
 def update_raw_data(frame):
     
+    global rangei,v1min,v1max,v2min,v2max,v3min,v3max
+
     myquote = kite.quote([scriptName])
     v1.append(float(myquote[scriptName]['buy_quantity']))
     v2.append(float(myquote[scriptName]['sell_quantity']))
@@ -56,21 +60,37 @@ def update_raw_data(frame):
     ax2_raw.clear()
     ax3_raw.clear()
 
+    
+    if rangei==0:
+        v1min,v1max = giveyrange(v1_data,0.6,1.4)
+        v2min,v2max = giveyrange(v2_data,0.6,1.4)
+        v3min,v3max = giveyrange(v3_data,0.99,1.01)
+        rangei+=1
+    
+    
+    if v1_data[-1] < v1min or v1_data[-1] > v1max:
+        v1min,v1max = giveyrange(v1_data,0.6,1.4)
+    if v2_data[-1] < v2min or v2_data[-1] > v2max:
+        v2min,v2max = giveyrange(v2_data,0.6,1.4)
+    if v3_data[-1] < v3min or v3_data[-1] > v3max:
+        v3min,v3max = giveyrange(v3_data,0.99,1.01)
+
+    print(f'min:{v3min} | max:{v3max}')
     # Plotting raw data for v1, v2, and v3
     ax1_raw.plot(x, v1_data, 'green')
     # ax1_raw.set_ylabel('v1', color='green')
     # ax1_raw.tick_params(axis='y', labelcolor='green')
-    ax1_raw.set_ylim(100000,500000)
+    ax1_raw.set_ylim(v1min,v1max)
 
     ax2_raw.plot(x, v2_data, 'red')
     # ax2_raw.set_ylabel('v2', color='red')
     # ax2_raw.tick_params(axis='y', labelcolor='red')
-    ax2_raw.set_ylim(100000,500000)
+    ax2_raw.set_ylim(v2min,v2max)
 
     ax3_raw.plot(x, v3_data, 'black')
     # ax3_raw.set_ylabel('v3', color='black')
     # ax3_raw.tick_params(axis='y', labelcolor='black')
-    ax3_raw.set_ylim(28000000,29000000)
+    ax3_raw.set_ylim(v3min,v3max)
 
 
 # Function to update percentage change
@@ -94,21 +114,21 @@ def update_percentage_change(frame):
 
     x_adjusted = x[1:]
 
-    # # Plotting percentage change for v1, v2, and v3
-    # ax1_pc.plot(x_adjusted, v1_pc[-len(x):], 'green')
-    # # ax1_pc.set_ylabel('v1 % Change', color='green')
-    # # ax1_pc.tick_params(axis='y', labelcolor='green')
-    # ax1_pc.set_ylim(-10, 10)
+    # Plotting percentage change for v1, v2, and v3
+    ax1_pc.plot(x_adjusted, v1_pc[-len(x):], 'green')
+    # ax1_pc.set_ylabel('v1 % Change', color='green')
+    # ax1_pc.tick_params(axis='y', labelcolor='green')
+    ax1_pc.set_ylim(-50, 50)
 
-    # ax2_pc.plot(x_adjusted, v2_pc[-len(x):], 'red')
-    # # ax2_pc.set_ylabel('v2 % Change', color='red')
-    # # ax2_pc.tick_params(axis='y', labelcolor='red')
-    # ax2_pc.set_ylim(-10, 10)
+    ax2_pc.plot(x_adjusted, v2_pc[-len(x):], 'red')
+    # ax2_pc.set_ylabel('v2 % Change', color='red')
+    # ax2_pc.tick_params(axis='y', labelcolor='red')
+    ax2_pc.set_ylim(-50, 50)
 
     ax3_pc.plot(x_adjusted, v3_pc[-len(x):], 'black')
     # ax3_pc.set_ylabel('v3 % Change', color='black')
     # ax3_pc.tick_params(axis='y', labelcolor='black')
-    ax3_pc.set_ylim(-1, 1)
+    ax3_pc.set_ylim(-0.5, 0.5)
 
 # Creating two separate figures for raw data and percentage change
 fig_raw, ax1_raw = plt.subplots(figsize=(5, 3))
